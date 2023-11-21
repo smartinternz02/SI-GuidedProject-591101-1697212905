867cd89b34256c9842f4471a73878b8601ca5536
Rati Dubey <95397109+dubeyrati@users.noreply.github.com>
2023-11-21 14:24:38 +0530

    Add files via upload

---
+++ Project Development Phase/SpillTheTea-app/settings.gradle.kts
@@ -0,0 +1,22 @@
+import org.gradle.kotlin.dsl.support.classPathBytesRepositoryFor
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io" ) }
+    }
+}
+rootProject.name = "SpillTheTea"
+include(":app")
+ 
